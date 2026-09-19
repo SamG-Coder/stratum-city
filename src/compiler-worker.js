9a36931cd26e654996e636f9a65e05bcd52c79df
Sam <13449963+SamG-Coder@users.noreply.github.com>
2026-09-19 16:59:48 +1000

    Fix compiler worker module newline

--- src/compiler-worker.js
+++ src/compiler-worker.js
@@ -1,2 +1,3 @@
-import {compile} from '../vendor/cuda-webshader/compiler/compiler.js';\nimport {compilerOptions} from './kernel-specs.js';
+import {compile} from '../vendor/cuda-webshader/compiler/compiler.js';
+import {compilerOptions} from './kernel-specs.js';
 self.onmessage=event=>{const {id,source,spec}=event.data;try{const {ast,kernel,...artifact}=compile(source,compilerOptions(spec));self.postMessage({id,artifact});}catch(e){self.postMessage({id,error:String(e.stack||e)});}};
